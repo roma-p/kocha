f07aebd6aae983ba6fc0e06af608c08e09e921ee
roma_p <pelle.romain@gmail.com>
2022-12-15 23:55:58 +0100

    initial commit.

---
+++ engine/src/defines.h
@@ -0,0 +1,104 @@
+#pragma once
+
+// unsigned int types
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+// signed int types
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
+
+// floating point types
+typedef float f32;
+typedef double f64;
+
+// boolean types
+typedef int b32; 
+typedef char b8; 
+
+
+// definitions of static assertions.
+#if defined(__clang__) || defined (__gcc__)
+#define STATIC_ASSERT _Static_assert
+#else
+#define STATIC_ASSERT static_assert
+#endif
+
+
+// asserting all types have the excepted size.
+STATIC_ASSERT(sizeof(u8)  == 1, "Expected u8 to be 1 byte.");
+STATIC_ASSERT(sizeof(u16) == 2, "Expected u8 to be 2 bytes.");
+STATIC_ASSERT(sizeof(u32) == 4, "Expected u8 to be 4 bytes");
+STATIC_ASSERT(sizeof(u64) == 8, "Expected u8 to be 8 bytes.");
+
+STATIC_ASSERT(sizeof(i8)  == 1, "Expected i8 to be 1 byte.");
+STATIC_ASSERT(sizeof(i16) == 2, "Expected i8 to be 2 bytes.");
+STATIC_ASSERT(sizeof(i32) == 4, "Expected i8 to be 4 bytes");
+STATIC_ASSERT(sizeof(i64) == 8, "Expected i8 to be 8 bytes.");
+
+STATIC_ASSERT(sizeof(f32) == 4, "Expected f32 to be 4 bytes.");
+STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
+
+#define TRUE  1
+#define FALSE 0  
+
+
+// PLateform detection 
+
+// WINDOWS
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#define KPLATFORM_WINDOWS 1
+#ifndef _WIN64
+#error "64 bit is required on Windows!"
+#endif
+
+// LINUX 
+#elif defined(__linux__) || defined(__gnu_linux__)
+#define KPLATFORM_LINUX 1
+#if defined(__ANDROID__)
+#define KPLATFORM_ANDROID 1
+#endif
+#elif defined(__unix__)
+#define KPLATFORM_UNIX = 1
+#elif defined(__POSIX_VERSION)
+#define KPLATFORM_POSIX = 1
+
+// APPLE
+#elif __APPLE__
+#define KPLATFORM_APPLE = 1
+#include <TargetConditionals.h>
+#if TARGET_IPHONE_SIMULATOR
+#define KPLATFORM_IOS 1
+#define KPLATFORM_IOS_SIMULATOR 1
+#elif TARGET_OS_IPHONE
+#define KPLATFORM_IOS 1
+#elif TARGET_OS_MAC
+/// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#else
+#error "Unknown Apple platform"
+#endif
+#else
+#error "Unknown platform"
+#endif
+
+// EXPORT
+#ifdef KEXPORT
+#ifdef _MSC_VER
+#define KAPI __declspec(dllexport)
+#else
+#define KAPI __attribute__((visibility("default")))
+#endif
+
+// IMPORT
+#else
+#ifdef _MSC_VER
+#define KAPI __declspec(dllimport)
+#else
+#define KAPI
+#endif
+#endif
+
---
+++ etc/j.md
@@ -0,0 +1,10 @@
+
+# 15/12/22 -------------------------------------------------------------------
+
+custom makefiles made from various pages... 
+minimal requirements are: 
+    - 2 makefiles: 1 for engine and 1 for app (unlike How to setup Vulkan)
+    - MacOs support but code ready to add support for linux / windows.
+    - No extra build.sh build_all.sh
+    - compile / link etc... command of Kohi are not ok.
+
