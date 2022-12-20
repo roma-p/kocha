516b460e7bc4bc6c1ad99e64ee05277e07b7d796
roma_p <pelle.romain@gmail.com>
2022-12-20 23:14:44 +0100

    exposed func in platform.h

--- engine/src/platform/platform.h
+++ engine/src/platform/platform.h
@@ -5,16 +5,13 @@ typedef struct platform_state {
     void* internal_state;
 } platform_state;
 
-b8 platform_startup(
+KAPI b8 platform_startup(
 	platform_state* plat_state,
 	const char* application_name, 
-	i32 x,
-	i32 y,
-	i32 width,
-	i32 height);
-
-void platform_shutdown(platform_state* plat_state);
-void platform_messages(platform_state* plat_state);
+	i32 x, i32 y,
+	i32 width, i32 height);
+KAPI void platform_shutdown(platform_state* plat_state);
+KAPI void platform_messages(platform_state* plat_state);
 
 void* platform_allocate(u64, b8 aligned);
 void  platform_free(void* black, b8 aligned);
