265d2273dbf177894c2befc0ec4ff99aed4365d0
roma_p <pelle.romain@gmail.com>
2022-12-21 10:42:29 +0100

    logger now make use of custom platform console write.

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,4 +1,5 @@
 #include "logger.h"
+#include "../platform/platform.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -23,19 +24,26 @@ void log_output(log_level level, const char* message, ...){
 	"[DEBUG]: ", 
 	"[TRACE]: ", 
     }; 
-    //b8 is_error = level < 2;
-    char out_message[32000];
-    char out_message_tmp[32000];
+
+    const i32 msg_length = 32000;
+    b8 is_error = level < LOG_LEVEL_WARN;
+
+    char out_message[msg_length];
+    char out_message_tmp[msg_length];
     memset(out_message, 0, sizeof(out_message));
     memset(out_message, 0, sizeof(out_message_tmp));
 
     __builtin_va_list p_arg;
     va_start(p_arg, message);
-    vsnprintf(out_message_tmp, 32000, message, p_arg);
+    vsnprintf(out_message_tmp, msg_length, message, p_arg);
     va_end(p_arg);
 
     sprintf(out_message, "%s%s\n", level_strings[level], out_message_tmp);
-
-    printf("%s", out_message);
+    
+    if (is_error) {
+	platform_console_write_error(out_message, level);
+    } else {
+	platform_console_write(out_message, level);
+    }
 }
 
